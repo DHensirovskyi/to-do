4590dd622736baee94eb848cdf259e0e0048f108
Dmytro Hensirovskyi <dmytrohensirovskyi@dhcp-pc118.rapidnet.de>
2025-10-08 13:28:01 +0200

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+  weight: ['300', '400', '500', '700'],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${montserrat.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
